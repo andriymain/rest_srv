28472edfe99653dd1f8a7f928c3bd819068e7068
user <user@i5>
2016-12-09 23:32:57 +0200

    Changes strings

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,10 @@
 
 public class Test2 {
 	private String str;
+	private String branch = "bug-123 master";
+	private String branch2 = "bug-123 - 2 master";
+	private String branch3 = "bug-123 - 3 master";
+	private String branch4 = "bug-123 - 4 master";
 
 	public String getStr() {
 		return str;
