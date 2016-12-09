5c34732371b4ccdd49a84ac64417c240b043e51f
user <user@i5>
2016-12-10 00:50:35 +0200

    777 comit 1

--- src/Test2.java
+++ src/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	private String branch2 = "bug-123 - 2 master";
 	private String branch3 = "bug-123 - 3 master";
 	private String branch4 = "bug-123 - 4 master";
+	private String branch5 = "bug-777 - 1";
 
 	public String getStr() {
 		return str;
