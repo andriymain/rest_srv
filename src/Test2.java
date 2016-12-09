f5f35e0bbb7e900492c32878101c0727f827e6b9
user <user@i5>
2016-12-09 23:30:37 +0200

    Added String bug-123 - 4 and pushed

--- src/Test2.java
+++ src/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	private String branch = "bug-123";
 	private String branch2 = "bug-123 - 2";
 	private String branch3 = "bug-123 - 3";
+	private String branch4 = "bug-123 - 4";
 
 	public String getStr() {
 		return str;
