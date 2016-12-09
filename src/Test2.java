b17b2aaf4767e39a57a1a7f615ea383a91ea0cce
user <user@i5>
2016-12-10 00:52:52 +0200

    Master addded magic method

--- src/Test2.java
+++ src/Test2.java
@@ -5,6 +5,8 @@ public class Test2 {
 	private String branch2 = "bug-123 - 2 master";
 	private String branch3 = "bug-123 - 3 master";
 	private String branch4 = "bug-123 - 4 master";
+	
+	private String branch5 = "bug-123 - master";
 
 	public String getStr() {
 		return str;
@@ -19,6 +21,10 @@ public class Test2 {
 	}
 	
 	public void newMthod() {
+		//bbbbbbbbb
+	}
+	
+	public void magicMethod() {
 		//bbbbbbbbb
 	}
 }
